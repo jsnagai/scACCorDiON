24d69ef88c25ac4df688ee4ec8f3e8bb3de42a62
jsnagai <james.nagai@gmail.com>
2023-11-07 15:34:36 +0100

    cleaning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,16 +12,27 @@ author = 'James S. Nagai'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- General configuration
 
-extensions = ["myst_parser"]
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    "myst_parser",
+    "nbsphinx", ## for jupyter notebook
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
+html_theme = 'sphinx_rtd_theme'
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
